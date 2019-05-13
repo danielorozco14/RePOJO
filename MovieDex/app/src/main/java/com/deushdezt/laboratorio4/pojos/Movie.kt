c6b2d08d4ce470ecbc370e3f225dba875be745a3
carlosjosetri <00082217@uca.edu.sv>
2019-05-13 07:46:58 -0600

    arreglado parce

--- MovieDex/app/src/main/java/com/deushdezt/laboratorio4/pojos/Movie.kt
+++ MovieDex/app/src/main/java/com/deushdezt/laboratorio4/pojos/Movie.kt
@@ -3,48 +3,50 @@ package com.deushdezt.laboratorio4.pojos
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Movie (
-        val name:String = "N/A",
-        val country:String = "N/A",
-        val value: String = "N/A",
-        val value_us:String = "N/A",
-        val year:String = "N/A",
-        val review:String = "N/A",
-        val isAvailable:String = "N/A",
-        val img:String = "N/A",
-        val _id:String = "N/A",
-        val _v:String = "N/A"
-    ) : Parcelable {
-        constructor(parcel: Parcel) : this(
-            name = parcel.readString(),
-            country = parcel.readString(),
-            value = parcel.readString(),
-            value_us = parcel.readString(),
-            year = parcel.readString(),
-            review = parcel.readString(),
-            isAvailable = parcel.readString(),
-            img = parcel.readString(),
-            _id= parcel.readString(),
-            __v = parcel.readString())
+data class Movie(
+    val name: String = "N/A",
+    val country: String = "N/A",
+    val value: Int = 0,
+    val value_us: Int = 0,
+    val year: Int = 9999,
+    val review: String = "N/A",
+    val isAvailable: Int = 0,
+    val img: String = "N/A",
+    val _id: String = "N/A",
+    val _v: Int = 0
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt()
+    )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(name)
         parcel.writeString(country)
-        parcel.writeString(value)
-        parcel.writeString(year)
-        parcel.writeString(value_us)
+        parcel.writeInt(value)
+        parcel.writeInt(year)
+        parcel.writeInt(value_us)
         parcel.writeString(review)
-        parcel.writeString(isAvailable)
+        parcel.writeInt(isAvailable)
         parcel.writeString(img)
         parcel.writeString(_id)
-        parcel.writeString(_v)
+        parcel.writeInt(_v)
 
     }
 
     override fun describeContents() = 0
 
     companion object {
-        @JvmField val CREATOR = object : Parcelable.Creator<Movie> {
+        @JvmField
+        val CREATOR = object : Parcelable.Creator<Movie> {
             override fun createFromParcel(parcel: Parcel): Movie = Movie(parcel)
             override fun newArray(size: Int): Array<Movie?> = arrayOfNulls(size)
         }
